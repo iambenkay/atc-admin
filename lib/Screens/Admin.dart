1dcff028ea1ff0e39418a37e196661a9e838c057
Benjamin <benjamincath@gmail.com>
2020-02-25 00:28:16 +0100

    Feature/ Implemented pinned greeting header

--- lib/Screens/Admin.dart
+++ lib/Screens/Admin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/greetings_header.dart';
 
 class Admin extends StatefulWidget {
   @override
@@ -51,208 +52,153 @@ class _AdminState extends State<Admin> {
               fit: BoxFit.cover,
             )),
             child: Container(
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Color(0x99ffffff),
-                ),
-                child: ListView(
-                  children: <Widget>[
-                    Card(
-                        margin: EdgeInsets.all(10),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        elevation: 3,
-                        child: Container(
-                            height: 220,
-                            child: Column(
-                              children: <Widget>[
-                                Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: EdgeInsets.only(left: 20),
-                                        child: Text(
-                                          "24th February",
-                                          style: TextStyle(
-                                              fontFamily: "SFUIDisplay",
-                                              fontSize: 30,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                      Container(
-                                        child: Padding(
-                                          padding: EdgeInsets.all(20),
-                                          child: CircleAvatar(
-                                            backgroundColor: Colors.black,
-                                            backgroundImage:
-                                                AssetImage("Assets/avatar.png"),
-                                            radius: 23,
-                                          ),
-                                        ),
-                                        alignment: Alignment.centerRight,
-                                      ),
-                                    ]),
-                                Container(
-                                  alignment: Alignment.centerLeft,
-                                  padding:
-                                      EdgeInsets.only(left: 20, bottom: 20),
-                                  child: Text(
-                                    "Welcome, Benjamin!",
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Color(0x99ffffff),
+              ),
+              child: CustomScrollView(
+                slivers: <Widget>[
+                  SliverPersistentHeader(
+                    pinned: true,
+                    delegate: GreetingsHeader(),
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      <Widget>[
+                        Card(
+                          color: Color(0xff446688),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                          child: Container(
+                            height: 150,
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 20, bottom: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Text("Charmaine TV",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 25,
+                                          fontFamily: "SFUIDisplay",
+                                          fontWeight: FontWeight.bold)),
+                                  Text(
+                                    "50 Channels",
                                     style: TextStyle(
-                                        fontFamily: 'SFUIDisplay',
-                                        fontSize: 28),
-                                  ),
-                                ),
-                                Container(
-                                    margin: EdgeInsets.only(top: 30),
-                                    child: Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                            child: Container(
-                                                child: Icon(Icons.show_chart))),
-                                        Expanded(
-                                            child: Container(
-                                                child: Icon(
-                                                    Icons.panorama_fish_eye))),
-                                        Expanded(
-                                            child: Container(
-                                                child:
-                                                    Icon(Icons.all_inclusive))),
-                                      ],
-                                    )),
-                              ],
-                            ))),
-                    Card(
-                      color: Color(0xff446688),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                      child: Container(
-                        height: 150,
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 20, bottom: 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Text("Charmaine TV",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 25,
-                                      fontFamily: "SFUIDisplay",
-                                      fontWeight: FontWeight.bold)),
-                              Text(
-                                "50 Channels",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontFamily: "SFUIDisplay"),
-                              )
-                            ],
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontFamily: "SFUIDisplay"),
+                                  )
+                                ],
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    Card(
-                      color: Color(0xff886644),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                      child: Container(
-                        height: 150,
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 20, bottom: 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Text("Charmaine Articles",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 25,
-                                      fontFamily: "SFUIDisplay",
-                                      fontWeight: FontWeight.bold)),
-                              Text(
-                                "50 Channels",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontFamily: "SFUIDisplay"),
-                              )
-                            ],
+                        Card(
+                          color: Color(0xff886644),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                          child: Container(
+                            height: 150,
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 20, bottom: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Text("Charmaine Articles",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 25,
+                                          fontFamily: "SFUIDisplay",
+                                          fontWeight: FontWeight.bold)),
+                                  Text(
+                                    "50 Channels",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontFamily: "SFUIDisplay"),
+                                  )
+                                ],
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    Card(
-                      color: Color(0xff448866),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                      child: Container(
-                        height: 150,
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 20, bottom: 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Text("Charmaine Shop",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 25,
-                                      fontFamily: "SFUIDisplay",
-                                      fontWeight: FontWeight.bold)),
-                              Text(
-                                "50 Channels",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontFamily: "SFUIDisplay"),
-                              )
-                            ],
+                        Card(
+                          color: Color(0xff448866),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                          child: Container(
+                            height: 150,
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 20, bottom: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Text("Charmaine Shop",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 25,
+                                          fontFamily: "SFUIDisplay",
+                                          fontWeight: FontWeight.bold)),
+                                  Text(
+                                    "50 Channels",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontFamily: "SFUIDisplay"),
+                                  )
+                                ],
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    Card(
-                      color: Color(0xff664488),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                      child: Container(
-                        height: 150,
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 20, bottom: 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Text("Charmaine Events",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 25,
-                                      fontFamily: "SFUIDisplay",
-                                      fontWeight: FontWeight.bold)),
-                              Text(
-                                "50 Channels",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontFamily: "SFUIDisplay"),
-                              )
-                            ],
+                        Card(
+                          color: Color(0xff664488),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                          child: Container(
+                            height: 150,
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 20, bottom: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Text("Charmaine Events",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 25,
+                                          fontFamily: "SFUIDisplay",
+                                          fontWeight: FontWeight.bold)),
+                                  Text(
+                                    "50 Channels",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontFamily: "SFUIDisplay"),
+                                  )
+                                ],
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
-                ))));
+                  )
+                ],
+              ),
+            )));
   }
 }
---
+++ lib/widgets/greetings_header.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class GreetingsHeader extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(BuildContext context, double offset, bool overlaps) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        print(offset);
+        String _greetings = "Welcome, Benjamin!", _date = "24th February";
+        return Card(
+            margin: EdgeInsets.all(10),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            elevation: 3,
+            child: Container(
+                height: constraints.maxHeight,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(left: 20),
+                            child: Text(
+                              _date,
+                              style: TextStyle(
+                                  fontFamily: "SFUIDisplay",
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(20),
+                              child: CircleAvatar(
+                                backgroundColor: Colors.black,
+                                backgroundImage:
+                                    AssetImage("Assets/avatar.png"),
+                                radius: 20,
+                              ),
+                            ),
+                            alignment: Alignment.centerRight,
+                          ),
+                        ]),
+                    Visibility(
+                      visible: offset < 65,
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.only(left: 20, bottom: 10),
+                        child: Text(
+                          _greetings,
+                          style: TextStyle(
+                              fontFamily: 'SFUIDisplay', fontSize: 28),
+                        ),
+                      ),
+                    ),
+                    Visibility(
+                      visible: offset < 10,
+                      child: Container(
+                          margin: EdgeInsets.only(top: 30),
+                          child: Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child:
+                                      Container(child: Icon(Icons.show_chart))),
+                              Expanded(
+                                  child: Container(
+                                      child: Icon(Icons.panorama_fish_eye))),
+                              Expanded(
+                                  child: Container(
+                                      child: Icon(Icons.all_inclusive))),
+                            ],
+                          )),
+                    ),
+                  ],
+                )));
+      },
+    );
+  }
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate _) => true;
+
+  @override
+  double get maxExtent => 220.0;
+  double get minExtent => 100.0;
+}
